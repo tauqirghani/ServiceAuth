45e9e8a7b15c4d56c99c7aef7a24ae4703952296
Tauqir Chaudhry <tchaudhry@firstam.com>
2020-05-27 11:13:16 -0700

    Adding Client API Project

---
+++ ClientAPI/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.AzureAppServices;
+
+namespace ClientAPI
+{
+    public class Program
+    {
+        public static ILogger Logger;
+        public static void Main(string[] args)
+        {
+            var Host = CreateHostBuilder(args).Build();
+
+            var LoggerF = LoggerFactory.Create(builder =>
+            {
+                builder.AddConsole().AddAzureWebAppDiagnostics()
+                .AddFilter("Microsoft", LogLevel.Warning)
+                .AddFilter("System", LogLevel.Warning);
+            });
+            Logger = LoggerF.CreateLogger<Program>();
+            Host.Run();
+
+
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging => logging.AddAzureWebAppDiagnostics())
+                .ConfigureServices(serviceCollection => serviceCollection
+                    .Configure<AzureFileLoggerOptions>(options =>
+                    {
+                        options.FileName = "azure-diagnostics-";
+                        options.FileSizeLimit = 50 * 1024;
+                        options.RetainedFileCountLimit = 5;
+                    }).Configure<AzureBlobLoggerOptions>(options =>
+                    {
+                        options.BlobName = "log.txt";
+                    })
+            )
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ ClientAPI/Startup.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Net.Http;
+using Microsoft.OpenApi.Models;
+
+namespace ClientAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+            services.AddSingleton<IConfiguration>(Configuration);
+            HttpClient tokenClient = new HttpClient();
+            services.AddSingleton<HttpClient>(tokenClient);
+            Uri targetUri = new Uri(Configuration["TargetURL"]);
+            ServicePointManager.FindServicePoint(targetUri).ConnectionLeaseTimeout = 60000;
+            HttpClient httpClient = new HttpClient()
+            {
+                BaseAddress = targetUri,
+            };
+            services.AddSingleton<HttpClient>(httpClient);
+            services.AddControllers();
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
