36af217d255cfce08b95705b3ec0df00739dda8f
Tauqir Chaudhry <tchaudhry@firstam.com>
2020-07-15 16:28:01 -0700

    Syntax Error correction

--- DataAPI/Controllers/FilesController.cs
+++ DataAPI/Controllers/FilesController.cs
@@ -11,7 +11,7 @@ namespace DataAPI.Controllers
 {
     [Route("data/v1/[controller]")]
     [ApiController]
-    [Authorize("Role="")]
+    [Authorize]
     public class FilesController : ControllerBase
     {
         private readonly FileContext _context;
